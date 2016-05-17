d6e423cbd10271ba5397b06e3fbc9b3f1fa56e13
Dmitry Erofeev <dmeroff@gmail.com>
2016-05-17 16:14:40 +0300

    Fix

--- migrations/m160516_202341_drop_organization_id_from_patient.php
+++ migrations/m160516_202341_drop_organization_id_from_patient.php
@@ -8,7 +8,10 @@ class m160516_202341_drop_organization_id_from_patient extends Migration
     {
         $this->dropForeignKey('fk_patient_organization', 'patient');
         $this->dropColumn('patient', 'organization_id');
-        $this->dropColumn('patient_archive', 'organization_id');
+        try {
+            $this->dropColumn('patient_archive', 'organization_id');    
+        } catch (Throwable $e) {
+        }
     }
 
     public function down()
