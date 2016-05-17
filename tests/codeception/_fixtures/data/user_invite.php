cb297d4eda72774944ece322387126631af3efcb
Dmitry Erofeev <dmeroff@gmail.com>
2016-05-18 00:06:38 +0300

    Update invite cest

--- tests/codeception/_fixtures/data/user_invite.php
+++ tests/codeception/_fixtures/data/user_invite.php
@@ -8,12 +8,14 @@ $faker = Factory::create();
 
 return [
     'chief_invite' => [
+        'id'         => 1,
         'email'      => $faker->email,
         'code'       => '1236547890',
         'created_at' => new Expression('NOW()'),
         'role'       => User::ROLE_CHIEF,
     ],
     'doctor_invite' => [
+        'id'          => 2,
         'email'       => $faker->email,
         'code'        => '9876543210',
         'created_at'  => new Expression('NOW()'),
@@ -21,8 +23,17 @@ return [
         'referrer_id' => 3,
     ],
     'patient_invite' => [
+        'id'          => 3,
         'email'       => $faker->email,
         'code'        => '03324569877',
+        'created_at'  => new Expression('NOW()'),
+        'role'        => User::ROLE_PATIENT,
+        'referrer_id' => 2,
+    ],
+    'patient2_invite' => [
+        'id'          => 4,
+        'email'       => $faker->email,
+        'code'        => '98716546455',
         'created_at'  => new Expression('NOW()'),
         'role'        => User::ROLE_PATIENT,
         'referrer_id' => 2,
--- tests/codeception/functional/InviteCest.php
+++ tests/codeception/functional/InviteCest.php
@@ -3,6 +3,7 @@
 use app\models\User;
 use app\models\UserInvite;
 use Faker\Factory;
+use yii\helpers\Json;
 
 class InviteCest
 {
@@ -27,5 +28,36 @@ class InviteCest
         // verify that there is only one invite with this code
         $count = UserInvite::find()->byCode($invite->code)->count();
         verify($count)->equals(1);
+    }
+
+    public function testInviteList(FunctionalTester $I)
+    {
+        $token = $I->getTokenFixture('doctor_auth_token')->code;
+
+        $I->amHttpAuthenticated($token, '');
+        $I->sendGET('invites');
+        $I->canSeeResponseCodeIs(200);
+        
+        $response = Json::decode($I->grabResponse());
+        $invite1  = $I->getInviteFixture('patient_invite');
+        $invite2  = $I->getInviteFixture('patient2_invite');
+
+        verify(count($response))->equals(2);
+        verify($response[0]['email'])->equals($invite1->email);
+        verify($response[1]['email'])->equals($invite2->email);
+    }
+
+    public function testInviteView(FunctionalTester $I)
+    {
+        $token  = $I->getTokenFixture('doctor_auth_token')->code;
+        $invite = $I->getInviteFixture('patient_invite');
+
+        $I->amHttpAuthenticated($token, '');
+        $I->sendGET('invites/' . $invite->id);
+        $I->canSeeResponseCodeIs(200);
+
+        $response = Json::decode($I->grabResponse());
+
+        verify($response['email'])->equals($invite->email);
     }
 }
